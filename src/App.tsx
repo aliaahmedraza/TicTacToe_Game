210337b285379598f8ca8b442f582df50f8abef5
aliaahmedraza <aliaahmedraza@gmail.com>
2025-05-30 15:40:40 +0500

    Add the winning score

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,21 @@ import GameBoard from './components/GameBoard';
 import { useTicTacToe } from './hooks/useTicTacToe';
 
 export default function App() {
-  const { board, currentPlayer, handleCellClick, resetGame } = useTicTacToe();
+  const { board, currentPlayer, handleCellClick, resetGame ,winnerCount,resetCounter} = useTicTacToe();
 
   return (
     <div className="flex flex-col justify-evenly items-center h-svh">
       <h1 className="text-5xl text-white font-bold mb-4">Tic-Tac-Toe</h1>
       <p className="text-black mb-2">Current Player: {currentPlayer}</p>
+      <div className='flex flex-row justify-center items-center gap-2'>     
+         <p className="text-black mb-2 text-2xl border-2">Player X Wins: {winnerCount.X} times.</p>
+         <p className="text-black mb-2 text-2xl border-2">Player O Wins: {winnerCount.O} times</p>
+      </div>
       <GameBoard board={board} onCellClick={handleCellClick} />
-      <button onClick={resetGame} className="mt-4 px-4 py-2 bg-white rounded">
+      <button onClick={() => {
+    resetGame();
+    resetCounter();
+   }}className="mt-4 px-4 py-2 bg-white rounded">
         Reset Game
       </button>
     </div>
--- src/hooks/useTicTacToe.ts
+++ src/hooks/useTicTacToe.ts
@@ -6,7 +6,19 @@ export const useTicTacToe = () => {
     Array(3).fill("").map(() => Array(3).fill(""))
   );
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
+  const [winnerCount, setWinnerCount] = useState<{ X: number; O: number }>({
+    X: 0,
+    O: 0,
+  });
 
+  const resetGame = () => {
+    setBoard(Array(3).fill("").map(() => Array(3).fill("")));
+    setCurrentPlayer("X");
+
+  };
+  const resetCounter=()=>{
+    setWinnerCount({ X: 0, O: 0 });
+  }
   const handleCellClick = (row: number, col: number) => {
     if (board[row][col] !== "") return;
 
@@ -15,18 +27,18 @@ export const useTicTacToe = () => {
     );
     setBoard(newBoard); 
     const winner = checkWinner(newBoard)
-    if (winner) {
-      setTimeout(()=>{
-        alert(`Player ${winner} Wins!`);resetGame()},500)
+    if (winner === "X" || winner === "O") {
+      // Dynamically update winner count
+      setWinnerCount(prev => ({ ...prev, [winner]: prev[winner] + 1 }));
+
+      setTimeout(() => {
+        alert(`Player ${winner} Wins!`);
+        resetGame();
+      }, 500);
     } else {
       setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
     }
   };
 
-  const resetGame = () => {
-    setBoard(Array(3).fill("").map(() => Array(3).fill("")));
-    setCurrentPlayer("X");
-  };
-
-  return { board, currentPlayer, handleCellClick, resetGame };
+  return { board, currentPlayer, handleCellClick, resetGame,winnerCount,resetCounter};
 };
